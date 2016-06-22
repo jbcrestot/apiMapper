b6a6d8d29acedbca7f5570bc5ebfcb2ec0e17b49
jbcrestot <jean-baptiste.crestot@canaltp.fr>
2016-06-22 18:17:44 +0200

    up

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,15 +4,20 @@ namespace AppBundle\Controller;
 
 use CanalTP\NavitiaPhp\Api\Coverage;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        dump($this->get('navitia.coverage')->all());
+        dump($this->get('navitia.coverage')->get());
 
+        dump($this->get('navitia.coverage')->findByCoords('2.37705;48.84675'));
 
-        $coverages = $this->get('navitia_coverage')->get();
-        dump($coverages);
+        dump($this->get('navitia.traffic_reports')->forCoverage());
+//        die;
+
 
         return $this->render('AppBundle::index.html.twig');
     }
